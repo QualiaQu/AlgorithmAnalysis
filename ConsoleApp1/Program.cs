dcc432491b569e4478f41048620cfac22dd4f040
Rustam <rustamdvl@yandex.ru>
2022-09-11 19:15:19 +0500

    345345

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+Stopwatch stopwatch = new Stopwatch();
+Random rand = new Random();
+int[] arr = new int[100];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(1, 3);
+}
+
+while (arr.Length != 0)
+{
+    for (int k = 0; k < 5; k++)
+    {
+        ulong sum = 1;
+        stopwatch.Restart();
+        for (int j = 0; j < arr.Length; j++)
+        {
+            sum *= (ulong)arr[j];
+        }
+        
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+        Console.WriteLine(sum);
+
+    }
+    Array.Resize(ref arr, arr.Length - 20);
+    Console.WriteLine("~~~~~~~~~~~~~~");
+
+}
