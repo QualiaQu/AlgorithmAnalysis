f19f05a8d3c7038d3492872a6ba24f734b1b9898
Rustam <rustamdvl@yandex.ru>
2022-09-16 13:42:40 +0500

    создана библиотека классов, начато разбиение алгоритмов в отдельные проекты

---
+++ ConstantFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using HelperLibrary;
+
+Stopwatch stopwatch = new Stopwatch();
+Random rand = new Random();
+int[] arr = new int[100000];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(1, 100);
+}
+string results = "Input data;Time (milliseconds)\n";
+
+while (arr.Length != 0)
+{
+    double averageTime = 0;
+    for (int k = 1; k <= 5; k++)
+    {
+        stopwatch.Restart();
+        int element = arr[0];
+        stopwatch.Stop();
+        averageTime += stopwatch.Elapsed.TotalMilliseconds;
+    }
+    averageTime /= 5;
+    results += $"{arr.Length};{(Math.Round(averageTime, 6)) * 1000}\n";
+    Array.Resize(ref arr, arr.Length - 10);
+}
+
+FileHelper.SaveResults(results);
---
+++ HelperLibrary/FileHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace HelperLibrary;
+
+public static class FileHelper
+{
+    public static void SaveResults(string results)
+    {
+        File.WriteAllText(Path.GetFullPath("./results.csv"), string.Empty);
+
+        Console.WriteLine(new String('-', 50));
+        File.AppendAllText(Path.GetFullPath("./results.csv"), results);
+        
+        Console.WriteLine("Результаты успешно сохранены!");
+    }
+}
